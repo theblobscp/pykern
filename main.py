fdc880664d990f55e095e09084841e6b13218776
NitrogenDioxide <45130240+Kiffolisk@users.noreply.github.com>
2021-11-16 20:13:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,18 +19,20 @@ def datafolder():
     return 'C:\\Users\\*\\' if os.name=="nt" else os.path.expanduser("~")
 
 output = ""
+Fore.LIGHTBLUE_EX
+THISISLIGHTGREENN = Fore.LIGHTYELLOW_EX
 osicon = [
-    " ################## ",
-    "#                  #",
-    "#  ######          #",
-    "#  #    #          #",
-    "#  ######          #",
-    "#  #        #   #  #",
-    "#  #         # #   #",
-    "#  #          #    #",
-    "#  #         #     #",
-    "#                  #",
-    " ################## "
+    Fore.LIGHTBLUE_EX + " ################## " + Style.RESET_ALL,
+    Fore.LIGHTBLUE_EX + "#                  #" + Style.RESET_ALL,
+    THISISLIGHTGREENN + "#  ######          #" + Style.RESET_ALL,
+    THISISLIGHTGREENN + "#  #    #          #" + Style.RESET_ALL,
+    Fore.LIGHTBLUE_EX + "#  ######          #" + Style.RESET_ALL,
+    Fore.LIGHTBLUE_EX + "#  #        #   #  #" + Style.RESET_ALL,
+    THISISLIGHTGREENN + "#  #         # #   #" + Style.RESET_ALL,
+    THISISLIGHTGREENN + "#  #          #    #" + Style.RESET_ALL,
+    Fore.LIGHTBLUE_EX + "#  #         #     #" + Style.RESET_ALL,
+    Fore.LIGHTBLUE_EX + "#                  #" + Style.RESET_ALL,
+    THISISLIGHTGREENN + " ################## " + Style.RESET_ALL
 ]
 
 def setPath(new):
@@ -58,6 +60,7 @@ def pykern():
                 for x in range(len(args)):
                     args[x] = args[x].replace("\"", "")
                 cmd = args[0]
+            
             ## filecheck
             cf = open(osdir + "/user/" + username + "/pkg/" + cmd + ".py", "r")
             cf.close()
@@ -94,101 +97,6 @@ def boot():
     username = uf.readline().lstrip().rstrip()
     uf.close()
     print("[x] Connected to user " + username)
-62
-            cf = open(osdir + "/user/" + username + "/pkg/" + cmd + ".py", "r")
-63
-            cf.close()
-64
-            ## actual command
-65
-​
-66
-            importantFunctions = [setPath]
-67
-            importantVars = [osdir, username, curdir, args, osicon] # do not change this order, you can add things to it
-68
-​
-69
-            spec = importlib.util.spec_from_file_location(cmd,osdir + "/user/" + username + "/pkg/" + cmd + ".py")
-70
-            cmdmod = importlib.util.module_from_spec(spec)
-71
-            spec.loader.exec_module(cmdmod)
-72
-            try:
-73
-                cmdmod.run(importantFunctions, importantVars)
-74
-            except Exception as error:
-75
-                print(Fore.LIGHTRED_EX + "ERROR EXECUTING COMMAND: " + error + Style.RESET_ALL)
-76
-        except:
-77
-            print(Fore.LIGHTRED_EX + "Failed to find command \"" + cmd + "\"" + Style.RESET_ALL)
-78
-​
-79
-def boot():
-80
-    global osdir
-81
-    global username
-82
-    global packagecount
-83
-    from setup import setupInit
-84
-    cls()
-85
-    if not exists(datafolder() + "/config.pykern"):
-86
-        setupInit()
-87
-    print("[-] Connecting installdir to PyKern instance")
-88
-    f = open(datafolder() + "/config.pykern", "r")
-89
-    osdir = f.readline()
-90
-    f.close()
-91
-    print("[x] Connected to " + osdir + ".")
-92
-    print("[-] Loading user")
-93
-    uf = open(osdir + "/user/.curuser", "r")
-94
-    username = uf.readline().lstrip().rstrip()
-95
-    uf.close()
-96
-    print("[x] Connected to user " + username)
-97
-    print("[-] Loading packages")
-98
-    pkgdir = osdir + "/user/" + username + "/pkg"
-99
-    for path in os.listdir(pkgdir):
-100
-        if os.path.isfile(os.path.join(pkgdir, path)):
-101
-            print("[-] - Loaded " + path.replace(".py", ""))
-102
-            packagecount += 1
-103
-    print("[x] Done, loaded " + str(packagecount) + " package(s).")
-104
-    cls()
-105
-    pykern()
-106
-​
-107
-print('Booting...')
-108
-boot()
-
     print("[-] Loading packages")
     pkgdir = osdir + "/user/" + username + "/pkg"
     for path in os.listdir(pkgdir):
